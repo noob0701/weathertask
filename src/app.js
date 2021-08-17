d5bc7ff7164c6d185a53e570f53076ab1d9cb5f8
Akshat Bahuguna <akshat250870@gmail.com>
2021-08-17 11:55:24 +0530

    ..

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs')
 const forecast = require('./utils/forecast')
 
 const app = express()
-
+const port = process.env.PORT || 3000
 const cors = require('cors');
 app.use(cors());
 
@@ -88,6 +88,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000.')
 })
